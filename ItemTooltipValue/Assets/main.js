da324067de4f7a1c720dd6dfb9f5c862af7e6cf5
Stefan Haneveer <s.haneveer93@gmail.com>
2018-02-06 22:59:06 +0100

    Initial commit

---
+++ ItemTooltipValue/Assets/main.js
@@ -0,0 +1,6 @@
+﻿stxl.modShowInventoryCurrencyTooltip = (priceValue) => {
+
+    const state = priceValue === '' ? 'none' : 'block';
+    $('#inventory_sellvalue_box').css('display', state);
+    $('#inventory_sellvalue_box #Petal_ValueDisplay').text(priceValue);
+};
---
+++ ItemTooltipValue/Patches/UpdateInventoryInfoPatch.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using Staxel;
+using Staxel.Client;
+using Staxel.Items;
+using Staxel.Items.ItemComponents;
+using Staxel.Rendering;
+
+namespace ItemTooltipValue.Patches
+{
+    [HarmonyPatch(typeof(InventoryController), "UpdateInventoryInfo")]
+    class UpdateInventoryInfoPatch
+    {
+        [HarmonyPostfix]
+        static void AfterUpdateInventoryInfo(Item item)
+        {
+            // WebOverlayRenderer is created after InventoryController (through OverlayController) so check just in case
+            WebOverlayRenderer overlay = ClientContext.WebOverlayRenderer;
+            if (overlay != null)
+            {
+                string sellPrice = item.GetComponents().Contains<PricingComponent>() ? item.GetComponents().Get<PricingComponent>().SellPrice.ToString() : "";
+                overlay.Call("modShowInventoryCurrencyTooltip", sellPrice, null, null, null, null, null);
+            }
+        }
+    }
+}
